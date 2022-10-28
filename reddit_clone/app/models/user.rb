757a6dad48a9d7aa4dd59fe7a36d14b6c94bf196
Ethan Graham <ethangraham43@gmail.com>
2022-10-28 10:59:00 -0400

    started auth

---
+++ reddit_clone/app/models/user.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, :presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+    before_validation :ensure_session_token
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && .is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    
+end
