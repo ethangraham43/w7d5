25464e04ac668e9d6d63c67885b3db8aa7d09245
Ethan Graham <ethangraham43@gmail.com>
2022-10-28 11:50:09 -0400

    finished auth

--- reddit_clone/app/controllers/users_controller.rb
+++ reddit_clone/app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+    before_action :require_logged_in, only: [:show, :index]
+    def show
+        @user = User.find(user_params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            redirect_to users_url
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
